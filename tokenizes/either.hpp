38a295083f150279715abff5458809bcd8078306
terurin <hamusutaateru@live.jp>
2023-06-11 08:09:12 +0000

    add map on either

--- tokenizes/either.hpp
+++ tokenizes/either.hpp
@@ -35,29 +35,14 @@ class either {
     either_mode mode;
     std::byte memory[max_size];
 
-    R *get_right_ptr() { return (R *)memory; }
-    const R *get_right_ptr() const { return (R *)memory; }
-    L *get_left_ptr() { return (L *)&memory; }
-    const L *get_left_ptr() const { return (L *)memory; }
-
     // allocate
     void allocate() { mode = either_mode::none; }
-    void allocate(const R &right) {
+    void allocate(R right) {
         new (memory) R(right);
         mode = either_mode::right;
     }
 
-    void allocate(R &&right) {
-        new (memory) R(right);
-        mode = either_mode::right;
-    }
-
-    void allocate(const L &left) {
-        new (memory) L(left);
-        mode = either_mode::left;
-    }
-
-    void allocate(L &&left) {
+    void allocate(L left) {
         new (memory) L(left);
         mode = either_mode::left;
     }
@@ -66,10 +51,10 @@ class either {
         using enum either_mode;
         switch (origin.mode) {
         case right:
-            allocate(*origin.get_right_ptr());
+            allocate(origin.get_right());
             break;
         case left:
-            allocate(*origin.get_left_ptr());
+            allocate(origin.get_left());
             break;
         case none:
             allocate();
@@ -81,10 +66,10 @@ class either {
         using enum either_mode;
         switch (origin.mode) {
         case right:
-            allocate(std::move(*origin.get_right_ptr()));
+            allocate(origin.into_right());
             break;
         case left:
-            allocate(std::move(*origin.get_left_ptr()));
+            allocate(origin.into_left());
             break;
         case none:
             allocate();
@@ -98,11 +83,11 @@ class either {
         using enum either_mode;
         switch (mode) {
         case right:
-            get_right_ptr()->~R();
+            get_right().~R();
             mode = none;
             return;
         case left:
-            get_left_ptr()->~L();
+            get_left().~L();
             mode = none;
             return;
         case none:
@@ -138,22 +123,22 @@ public:
 
     either &operator=(const right<R> &_right) {
         release();
-        allocate(_right);
+        allocate(_right.value);
         return *this;
     }
     either &operator=(right<R> &&_right) {
         release();
-        allocate(std::move(_right));
+        allocate(std::move(_right.value));
         return *this;
     }
     either &operator=(const left<L> &_left) {
         release();
-        allocate(_left);
+        allocate(_left.value);
         return *this;
     }
     either &operator=(left<L> &&_left) {
         release();
-        allocate(std::move(_left));
+        allocate(std::move(_left.value));
         return *this;
     }
 
@@ -171,41 +156,52 @@ public:
         if (mode != either_mode::right) {
             return std::nullopt;
         }
-        return *get_right_ptr();
+        return *(R *)memory;
     }
     std::optional<L> opt_left() const {
         if (mode != either_mode::left) {
             return std::nullopt;
         }
-        return *get_left_ptr();
+        return *(L *)memory;
     }
     // get-*
     R &get_right() {
         if (!is_right()) {
             throw std::range_error("either is not right");
         }
-        return *get_right_ptr();
+        return *(R *)memory;
     }
     const R &get_right() const {
         if (!is_right()) {
             throw std::range_error("either is not right");
         }
-        return *get_right_ptr();
+        return *(R *)memory;
+    }
+    R &&into_right() {
+        if (!is_right()) {
+            throw std::range_error("either is not right");
+        }
+        return std::move(*(R *)memory);
     }
 
     L &get_left() {
         if (!is_left()) {
             throw std::range_error("either is not left");
         }
-        return *get_left_ptr();
+        return *(L *)memory;
     }
     const L &get_left() const {
         if (!is_left()) {
             throw std::range_error("either is not left");
         }
-        return *get_left_ptr();
+        return *(L *)memory;
     }
-
+    L &&into_left() {
+        if (!is_left()) {
+            throw std::range_error("either is not left");
+        }
+        return std::move(*(L *)memory);
+    }
     // *-or
 
     R get_right_or(R &&value) const {
@@ -238,7 +234,80 @@ public:
     }
 
     // *-map
+    template <std::invocable<const R&> F>
+    auto right_map(F &&func) {
+        using R2 = std::invoke_result_t<F, R>;
+        using E = either<R2, L>;
+        switch (mode) {
+        case either_mode::right:
+            return E(right<R2>(func(get_right())));
+        case either_mode::left:
+            return E(left<L>(get_left()));
+        case either_mode::none:
+            return E();
+        default:
+            throw std::range_error("unexpected mode");
+        }
+    }
+    template <std::invocable<const L&> F>
+    auto left_map(F &&func) {
+        using L2 = std::invoke_result_t<F, L>;
+        using E = either<R, L2>;
+        switch (mode) {
+        case either_mode::right:
+            return E(right(get_right()));
+        case either_mode::left:
+            return E(left(func(get_left())));
+        case either_mode::none:
+            return E();
+        default:
+            throw std::range_error("unexpected mode");
+        }
+    }
 
+    // into-*-map
+    template <std::invocable<R&&> F>
+    auto into_right_map(F &&func) {
+        using R2 = std::invoke_result_t<F, R>;
+        using E = either<R2, L>;
+        E result;
+        switch (mode) {
+        case either_mode::right:
+            result = E(right<R2>(func(into_right())));
+            break;
+        case either_mode::left:
+            result = E(left<L>(into_left()));
+            break;
+        case either_mode::none:
+            result = E();
+            break;
+        default:
+            throw std::range_error("unexpected mode");
+        }
+        release();
+        return result;
+    }
+    template <std::invocable<L&&> F>
+    auto into_left_map(F &&func) {
+        using L2 = std::invoke_result_t<F, L>;
+        using E = either<R, L2>;
+        E result;
+        switch (mode) {
+        case either_mode::right:
+            result = E(right(into_right()));
+            break;
+        case either_mode::left:
+            result = E(left(func(into_left())));
+            break;
+        case either_mode::none:
+            result = E();
+            break;
+        default:
+            throw std::range_error("unexpected mode");
+        }
+        release();
+        return result;
+    }
 };
 
 } // namespace tokenize::eithers
--- tokenizes/either_test.cpp
+++ tokenizes/either_test.cpp
@@ -5,49 +5,49 @@
 using namespace tokenize::eithers;
 
 TEST(either, right_prop) {
-    either<int,std::string> e= right(1);
-    
-    EXPECT_EQ(e.get_mode(),either_mode::right);
+    either<int, std::string> e = right(1);
+
+    EXPECT_EQ(e.get_mode(), either_mode::right);
     EXPECT_FALSE(e.is_none());
     EXPECT_TRUE(e.is_right());
     EXPECT_FALSE(e.is_left());
 
     // opt
-    EXPECT_EQ(e.opt_right(),1);
-    EXPECT_EQ(e.opt_left(),std::nullopt);
+    EXPECT_EQ(e.opt_right(), 1);
+    EXPECT_EQ(e.opt_left(), std::nullopt);
 
     // get
-    EXPECT_EQ(e.get_right(),1);
-    EXPECT_THROW(e.get_left(),std::range_error);
+    EXPECT_EQ(e.get_right(), 1);
+    EXPECT_THROW(e.get_left(), std::range_error);
 
     // get-*-or
-    EXPECT_EQ(e.get_right_or(2),1);
-    EXPECT_EQ(e.get_left_or("abc"),"abc");
+    EXPECT_EQ(e.get_right_or(2), 1);
+    EXPECT_EQ(e.get_left_or("abc"), "abc");
 }
 
 TEST(either, left_prop) {
-    either<int,std::string> e= left<std::string>("abc");
-    
-    EXPECT_EQ(e.get_mode(),either_mode::left);
+    either<int, std::string> e = left<std::string>("abc");
+
+    EXPECT_EQ(e.get_mode(), either_mode::left);
     EXPECT_FALSE(e.is_none());
     EXPECT_FALSE(e.is_right());
     EXPECT_TRUE(e.is_left());
 
     // opt
-    EXPECT_EQ(e.opt_right(),std::nullopt);
-    EXPECT_EQ(e.opt_left(),"abc");
+    EXPECT_EQ(e.opt_right(), std::nullopt);
+    EXPECT_EQ(e.opt_left(), "abc");
 
     // get
-    EXPECT_THROW(e.get_right(),std::range_error);
-    EXPECT_EQ(e.get_left(),"abc");
+    EXPECT_THROW(e.get_right(), std::range_error);
+    EXPECT_EQ(e.get_left(), "abc");
 
     // get-*-or
-    EXPECT_EQ(e.get_right_or(2),2);
-    EXPECT_EQ(e.get_left_or("xyz"),"abc");
+    EXPECT_EQ(e.get_right_or(2), 2);
+    EXPECT_EQ(e.get_left_or("xyz"), "abc");
 }
 
 TEST(either, reset) {
-    either<int,std::string> e= right(1);
+    either<int, std::string> e = right(1);
     e.reset();
     EXPECT_TRUE(e.is_none());
     EXPECT_FALSE(e.is_right());
@@ -55,28 +55,53 @@ TEST(either, reset) {
 }
 
 TEST(either, assign_either_right) {
-    either<int,std::string> e=right(1);
-    either<int,std::string> f;
-    f=e;
-    EXPECT_EQ(f.get_right(),1);
+    either<int, std::string> e = right(1);
+    either<int, std::string> f;
+    f = e;
+    EXPECT_EQ(f.get_right(), 1);
 }
 
 TEST(either, assign_either_left) {
-    either<int,std::string> e=left<std::string>("abc");
-    either<int,std::string> f;
-    f=e;
-    EXPECT_EQ(f.opt_left(),"abc");
+    either<int, std::string> e = left<std::string>("abc");
+    either<int, std::string> f;
+    f = e;
+    EXPECT_EQ(f.opt_left(), "abc");
 }
 
 TEST(either, assign_left) {
-    either<int,std::string> e;
-    e = left<std::string>("abc");   
-    EXPECT_EQ(e.opt_left(),"abc");
+    either<int, std::string> e;
+    e = left<std::string>("abc");
+    EXPECT_EQ(e.opt_left(), "abc");
 }
 
 TEST(either, assign_right) {
-    either<int,std::string> e;
+    either<int, std::string> e;
     e = right(1);
-    EXPECT_EQ(e.opt_right(),1);
+    EXPECT_EQ(e.opt_right(), 1);
 }
 
+TEST(either, right_map) {
+    either<int, std::string> e = right(10);
+    auto f = e.right_map([](const int& x) { return x + 10; });
+    EXPECT_EQ(f.opt_right(), 20);
+}
+
+TEST(either, left_map) {
+    either<int, std::string> e = left<std::string>("a");
+    auto f = e.left_map([](const std::string &x) { return x + "b"; });
+    EXPECT_EQ(f.opt_left(), "ab");
+}
+
+TEST(either, into_right_map) {
+    either<int, std::string> e = right(10);
+    auto f = e.into_right_map([](int&& x) { return x + 10; });
+    EXPECT_TRUE(e.is_none());
+    EXPECT_EQ(f.opt_right(), 20);
+}
+
+TEST(either, into_left_map) {
+    either<int, std::string> e = left<std::string>("a");
+    auto f = e.into_left_map([](std::string &&x) { return x + "b"; });
+    EXPECT_TRUE(e.is_none());
+    EXPECT_EQ(f.opt_left(), "ab");
+}
