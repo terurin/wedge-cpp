93327e3667b4ea547c43ca4d2dd47939a3995ae9
terurin <hamusutaateru@live.jp>
2023-06-14 18:16:46 +0000

    repeat: support either

--- tokenizes/repeats.hpp
+++ tokenizes/repeats.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
+#include "either.hpp"
 #include <functional>
 #include <istream>
 #include <optional>
 #include <vector>
 namespace tokenizes::repeats {
 
-template <class T, class C>
+using tokenizes::eithers::either;
+using tokenizes::eithers::either_mode;
+using tokenizes::eithers::left;
+using tokenizes::eithers::right;
+template <class R, class RC, class L>
 class repeat {
 public:
-    using parser_t = std::function<std::optional<T>(std::istream &)>;
+    using parser_t = std::function<either<R, L>(std::istream &)>;
 
 private:
     parser_t parser;
@@ -18,112 +23,104 @@ private:
 public:
     repeat(const parser_t &_parser, size_t _n = 0, size_t _m = SIZE_MAX) : parser(_parser), n(_n), m(_m) {}
     repeat(parser_t &&_parser, size_t _n = 0, size_t _m = SIZE_MAX) : parser(_parser), n(_n), m(_m) {}
-    std::optional<C> operator()(std::istream &is) const {
+    either<RC, L> operator()(std::istream &is) const {
         size_t i = 0;
-        C items;
+        RC items;
         // head
         for (const auto head = is.tellg(); i < n; i++) {
-            const std::optional<T> item = parser(is);
-            if (!item) {
+            const either<R, L> item = parser(is);
+            switch (item.get_mode()) {
+            case either_mode::right:
+                items.push_back(item.get_right());
+                break;
+            case either_mode::left:
                 is.seekg(head);
-                return std::nullopt;
+                return left<L>(item.get_left());
+            case either_mode::none:
+                throw std::range_error("none is unexpceted");
+            default:
+                throw std::range_error("others is unexpceted");
             }
-            items.push_back(*item);
         }
 
         // tail
         for (; i < m; i++) {
             const auto tail = is.tellg();
-            const std::optional<T> item = parser(is);
-            if (!item) {
+            const either<R, L> item = parser(is);
+            if (!item.is_right()) {
                 is.seekg(tail);
-                return items;
+                return right<RC>(items);
             }
-            items.push_back(*item);
+            items.push_back(item.get_right());
         }
-        return items;
+        return right<RC>(items);
     }
 };
 
 // T -> vector<T>
 template <class P>
-repeat(P, size_t, size_t) -> repeat<typename std::invoke_result_t<P, std::istream &>::value_type,
-                                    std::vector<typename std::invoke_result_t<P, std::istream &>::value_type>>;
+repeat(P, size_t, size_t) -> repeat<typename std::invoke_result_t<P, std::istream &>::right_t,
+                                    std::vector<typename std::invoke_result_t<P, std::istream &>::right_t>,
+                                    typename std::invoke_result_t<P, std::istream &>::left_t>;
 
 template <class P>
-concept char_parser = std::same_as<typename std::invoke_result_t<P, std::istream &>::value_type, char>;
+concept char_parser = std::same_as<typename std::invoke_result_t<P, std::istream &>::right_t, char>;
 
 // char -> std::string
 template <char_parser P>
-repeat(P, size_t, size_t) -> repeat<typename std::invoke_result_t<P, std::istream &>::value_type, std::string>;
+repeat(P, size_t, size_t) -> repeat<typename std::invoke_result_t<P, std::istream &>::right_t, std::string,
+                                    typename std::invoke_result_t<P, std::istream &>::left_t>;
 
 template <class P>
 static inline auto many0(const P &p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<T, std::vector<T>>(p, 0);
+    using R = std::invoke_result_t<P, std::istream &>::right_t;
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<R, std::vector<R>, L>(p, 0);
 }
 
 template <class P>
 static inline auto many0(P &&p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<T, std::vector<T>>(std::move(p), 0);
+    using R = std::invoke_result_t<P, std::istream &>::right_t;
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<R, std::vector<R>, L>(std::move(p), 0);
 }
 
 template <char_parser P>
 static inline auto many0(const P &p) {
-    return repeat<char, std::string>(p, 0);
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<char, std::string, L>(p, 0);
 }
 
 template <char_parser P>
 static inline auto many0(P &&p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<char, std::string>(std::move(p), 0);
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<char, std::string, L>(std::move(p), 0);
 }
 
 template <class P>
 static inline auto many1(const P &p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<T, std::vector<T>>(p, 1);
+    using R = std::invoke_result_t<P, std::istream &>::right_t;
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<R, std::vector<R>, L>(p, 1);
 }
 
 template <class P>
 static inline auto many1(P &&p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<T, std::vector<T>>(std::move(p), 1);
+    using R = std::invoke_result_t<P, std::istream &>::right_t;
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<R, std::vector<R>, L>(std::move(p), 1);
 }
 
 template <char_parser P>
 static inline auto many1(const P &p) {
-    return repeat<char, std::string>(p, 1);
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<char, std::string, L>(p, 1);
 }
 
 template <char_parser P>
 static inline auto many1(P &&p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<char, std::string>(std::move(p), 1);
-}
-
-template <class P>
-static inline auto some(const P &p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<T, std::vector<T>>(p, 0, 1);
-}
-
-template <class P>
-static inline auto some(P &&p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<T, std::vector<T>>(std::move(p), 0, 1);
-}
-
-template <char_parser P>
-static inline auto some(const P &p) {
-    return repeat<char, std::string>(p, 0, 1);
-}
-
-template <char_parser P>
-static inline auto some(P &&p) {
-    using T = std::invoke_result_t<P, std::istream &>::value_type;
-    return repeat<char, std::string>(std::move(p), 0, 1);
+    using L = std::invoke_result_t<P, std::istream &>::left_t;
+    return repeat<char, std::string, L>(std::move(p), 1);
 }
 
 } // namespace tokenizes::repeats
--- tokenizes/repeats_test.cpp
+++ tokenizes/repeats_test.cpp
@@ -14,25 +14,25 @@ const static auto parser = repeat(digit, 2, 4);
 TEST(repeat, digit_1) {
     stringstream ss;
     ss << "1";
-    EXPECT_EQ(parser(ss), std::nullopt);
+    EXPECT_EQ(parser(ss).opt_right(), std::nullopt);
 }
 
 TEST(repeat, digit_2) {
     stringstream ss;
     ss << "12";
-    EXPECT_EQ(parser(ss), "12");
+    EXPECT_EQ(parser(ss).opt_right(), "12");
 }
 
 TEST(repeat, digit_4) {
     stringstream ss;
     ss << "1234";
-    EXPECT_EQ(parser(ss), "1234");
+    EXPECT_EQ(parser(ss).opt_right(), "1234");
 }
 
 TEST(repeat, digit_5) {
     stringstream ss;
     ss << "12345";
-    EXPECT_EQ(parser(ss), "1234");
+    EXPECT_EQ(parser(ss).opt_right(), "1234");
 }
 
 } // namespace repeat_digit
@@ -43,19 +43,19 @@ const static auto parser = many0(digit);
 TEST(many0, digit_0) {
     stringstream ss;
     ss << "";
-    EXPECT_EQ(parser(ss), "");
+    EXPECT_EQ(parser(ss).opt_right(), "");
 }
 
 TEST(many0, digit_1) {
     stringstream ss;
     ss << "1";
-    EXPECT_EQ(parser(ss), "1");
+    EXPECT_EQ(parser(ss).opt_right(), "1");
 }
 
 TEST(many0, digit_2) {
     stringstream ss;
     ss << "12";
-    EXPECT_EQ(parser(ss), "12");
+    EXPECT_EQ(parser(ss).opt_right(), "12");
 }
 
 } // namespace many0
@@ -66,19 +66,19 @@ const static auto parser = many1(digit);
 TEST(many1, digit_0) {
     stringstream ss;
     ss << "";
-    EXPECT_EQ(parser(ss), std::nullopt);
+    EXPECT_EQ(parser(ss).opt_right(), std::nullopt);
 }
 
 TEST(many1, digit_1) {
     stringstream ss;
     ss << "1";
-    EXPECT_EQ(parser(ss), "1");
+    EXPECT_EQ(parser(ss).opt_right(), "1");
 }
 
 TEST(many1, digit_2) {
     stringstream ss;
     ss << "12";
-    EXPECT_EQ(parser(ss), "12");
+    EXPECT_EQ(parser(ss).opt_right(), "12");
 }
 
 } // namespace many0
