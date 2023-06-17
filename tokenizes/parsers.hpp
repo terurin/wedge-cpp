9d5b4093f232f5f0017efe66a73246f430c02467
terurin <hamusutaateru@live.jp>
2023-06-17 15:17:20 +0000

    paraser: add repeat&erase on shell

--- tokenizes/parsers.hpp
+++ tokenizes/parsers.hpp
@@ -22,8 +22,8 @@
 
 namespace tokenizes {
 
-using tokenizes::concepts::parsable;
 using tokenizes::concepts::left_of;
+using tokenizes::concepts::parsable;
 using tokenizes::concepts::right_of;
 using tokenizes::eithers::either;
 using tokenizes::eithers::either_mode;
@@ -45,6 +45,7 @@ public:
     shell(parser_t &&_parser) : parser(_parser) {}
     either<R, L> operator()(std::istream &is) const { return parser(is); }
 
+    // map_*
     template <class F>
     auto map_right(F &&func) const {
         return shell(mappers::mapper_right(*this, std::move(func)));
@@ -55,6 +56,7 @@ public:
         return shell(mappers::mapper_left(*this, std::move(func)));
     }
 
+    // const_*
     template <class V>
     auto const_right(V &&v) const {
         return shell(mappers::constant_right(*this, std::move(v)));
@@ -65,9 +67,15 @@ public:
         return shell(mappers::constant_left(*this, std::move(v)));
     }
 
+    // repeat, many
     auto repeat(size_t n, size_t m) const { return shell(repeats::repeat(*this, n, m)); }
     auto many0() const { return shell(repeats::many0(*this)); }
     auto many1() const { return shell(repeats::many1(*this)); }
+
+    // erase_*
+    auto erase_right() const { return shell(eraser_right(*this)); }
+    auto erase_left() const { return shell(eraser_left(*this)); }
+    auto erase_both() const { return shell(eraser_both(*this)); }
 };
 
 template <parsable P>
@@ -97,5 +105,13 @@ const static inline shell_char hexdigit = shell(primitive::hexdigit);
 
 // tag //
 static inline shell<std::string, nullptr_t> tag(std::string_view sv) { return shell(primitive::tag(sv)); }
+
+// tag_list
+static inline shell<std::string, nullptr_t> tag_list(std::vector<std::string> &items) {
+    return shell(primitive::tag_list(items));
+}
+static inline shell<std::string, nullptr_t> tag_list(std::initializer_list<std::string_view> items) {
+    return shell(primitive::tag_list(items));
+}
 
 } // namespace tokenizes
