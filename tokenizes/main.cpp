6ddaebf0270a7b642651d97ad12c400789a59a7e
terurin <hamusutaateru@live.jp>
2023-06-20 15:32:31 +0000

    primitive: add integer parser

--- tokenizes/main.cpp
+++ tokenizes/main.cpp
@@ -9,16 +9,12 @@ using namespace std;
 int main(int argc, char **argv) {
     stringstream ss;
     //ss << "hello_world";
-    ss << "hello_worlda";
-    ss.seekg(0);
+    ss << "-0b1111";
     char c;
 
-    std::vector<std::string> list = {"hello_world","hello", "hola", ""};
+    tokenizes::primitive::integer_parser parser;
 
-    tokenizes::primitive::tag_list tags(list);
-    cout << tags << endl;
-
-    if (auto e = tags(ss); e.is_right()) {
+    if (auto e = parser(ss); e.is_right()) {
         cout << e.get_right();
     } else {
         cout << "failed" << endl;
